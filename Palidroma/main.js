3f89068a537f7d9c62ac8fe3b247b235b0d1a6a3
MirunaMb <mirunamarcela98@gmail.com>
2023-07-27 17:24:18 +0200

    add logic

---
+++ Palidroma/main.js
@@ -0,0 +1,13 @@
+const word = prompt("Inserisci una parola di 4 lettere")
+// console.log(word[0]);
+for ( let i = 0; i < word.length; i++){
+    const letter = word[i] ;
+    // console.log(letter);
+}
+
+for (let i = 3; i < word.length; i--){
+    const letter = word[i];
+    console.log(letter);
+    
+}
+const reverseWord = reverseWord + letter ;
